77f6e4c76e39aaa5584b0638ca1f63db969c46fc
Zaid Alhir <zaidalhir@yahoo.com>
2020-03-19 20:41:27 -0500

    creating middleware auth, chaning auth route

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const auth = require("../../middleware/auth");
 
 //@route  GET api/auth
 //@desc   Test rout
 //@access Public
-router.get("/", (req, res) => res.send("auth route"));
+router.get("/", auth, (req, res) => res.send("Auth route"));
 
 module.exports = router;
